21b0bb72934714055ce9180858bcedf5b0bb7c46
Jmeas <jellyes2@gmail.com>
2018-09-19 22:35:00 -0700

    Begin to support larger screens

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import HistoricalSuccess from './calculator/historical-success';
+import './app.css';
 
 class App extends Component {
   render() {
-    return <HistoricalSuccess />;
+    return (
+      <div className="app_body">
+        <HistoricalSuccess />
+      </div>
+    );
   }
 }
 
