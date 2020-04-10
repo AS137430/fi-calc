499bf8be80b6b7423f5b53affe345cd1b81ae74e
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-04-10 13:51:07 -0700

    Simplify results code

--- src/results/results.js
+++ src/results/results.js
@@ -1,15 +1,18 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
+import _ from 'lodash';
 import './results.css';
-import MultipleSimulations from './multiple-simulations';
 import computeResult from '../utils/compute-result';
 import usePortfolio from '../state/portfolio';
 import useSpendingPlan from '../state/spending-plan';
 import useLengthOfRetirement from '../state/length-of-retirement';
+import MultipleOverview from './multiple-overview';
+import OneCycle from './one-cycle';
 
 export default function Results({ goToConfig }) {
   const { state: spendingPlan } = useSpendingPlan();
   const { state: lengthOfRetirement } = useLengthOfRetirement();
   const { state: portfolio } = usePortfolio();
+  const [selectedStartYear, setSelectedStartYear] = useState(null);
 
   const result = useMemo(
     () => {
@@ -28,9 +31,42 @@ export default function Results({ goToConfig }) {
     ]
   );
 
+  useEffect(
+    () => {
+      window.scrollTo(0, 0);
+      setSelectedStartYear(null);
+    },
+    [result]
+  );
+
+  function updateStartYear(startYear) {
+    window.scrollTo(0, 0);
+    const target = _.find(result.results.allCycles, {
+      startYear,
+    });
+    setSelectedStartYear(target);
+  }
+
   return (
     <div className="results">
-      <MultipleSimulations result={result} goToConfig={goToConfig} />
+      {selectedStartYear === null && (
+        <MultipleOverview
+          goToConfig={goToConfig}
+          result={result}
+          updateStartYear={updateStartYear}
+        />
+      )}
+      {selectedStartYear && (
+        <OneCycle
+          goBack={() => {
+            window.scrollTo(0, 0);
+            setSelectedStartYear(null);
+          }}
+          inputs={result.inputs}
+          isSuccessful={!selectedStartYear.isFailed}
+          cycle={selectedStartYear}
+        />
+      )}
     </div>
   );
 }
