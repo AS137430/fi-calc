14fcd12928497564281ad323ed55d32f39e7a3dd
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-04-10 13:46:48 -0700

    Remove unused files; reorg utils

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { Router } from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory';
 import queryString from 'query-string';
 import './app.css';
-import historyWithQuery from './common/utils/history-with-query';
+import historyWithQuery from './utils/routing/history-with-query';
 import Configuration from './configuration/configuration';
 import Results from './results/results';
 import useIsSmallScreen from './hooks/use-is-small-screen';
