bee5460abf0d7e4ebec34ab7b8972a007696a2c9
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-04-10 18:02:34 -0700

    Convert inflation-from-cpi to TS+tests

--- src/utils/market-data/inflation-from-cpi.ts
+++ src/utils/market-data/inflation-from-cpi.ts
@@ -1,6 +1,14 @@
+interface InflationFromCpiOptions {
+  startCpi: number;
+  endCpi: number;
+}
+
 // Given `startCpi` and `endCpi`, returns the inflation.
 // This function can be used to compute the inflation between two
 // arbitrary years, such as 1871 and 1995.
-export default function inflationFromCpi({ startCpi, endCpi }) {
+export default function inflationFromCpi({
+  startCpi,
+  endCpi,
+}: InflationFromCpiOptions): number {
   return endCpi / startCpi;
 }
