98c765a1628bde31298e331cfd277de67a97fc55
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-04-18 15:22:45 -0700

    Remove unused code

--- src/form-config/length-of-retirement-form.js
+++ src/form-config/length-of-retirement-form.js
@@ -1,21 +1,12 @@
-import _ from 'lodash';
 import {
   isRequired,
   numberRequired,
   integerRequired,
-  // tooLarge,
   greaterThanZero,
   withinYearLimit,
   lessThanEndYear,
   greaterThanStartYear,
 } from '../utils/forms/validators';
-// import marketDataByYear from '../utils/market-data/market-data-by-year';
-
-// const marketData = marketDataByYear();
-
-// If the duration goes higher than the number of years in our market data, then no simulations complete,
-// and we cannot output any results.
-// const maxDuration = _.size(marketData) - 1;
 
 export default {
   values: {
@@ -27,7 +18,6 @@ export default {
         numberRequired,
         integerRequired,
         greaterThanZero,
-        // tooLarge(maxDuration),
       ],
     },
 
