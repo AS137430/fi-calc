6f8600e7228365627b50866d405dcfd0d4a843c7
James Smith <jamesplease2@gmail.com>
2020-04-17 22:15:58 -0700

    Reintroduce redirect logic

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import classnames from 'classnames';
-import { Switch, Route, useLocation } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import './app.css';
 import Nav from './common/nav';
 import Footer from './common/footer';
@@ -10,56 +10,9 @@ import Learn from './pages/learn';
 import Calculator from './pages/calculator';
 import NotFound from './common/not-found';
 import useIsCalculator from './hooks/use-is-calculator';
-import useIsSmallScreen from './hooks/use-is-small-screen';
-import useSimulationResult from './state/simulation-result';
 
 export default function App() {
-  const { result } = useSimulationResult();
-  const isSmallScreen = useIsSmallScreen();
-  const { pathname } = useLocation();
-  // const history = useHistory();
   const isCalculator = useIsCalculator();
-
-  // Right now, the app has no state persistence. Therefore,
-  // it doesn't make sense to allow the user to visit any other URL other
-  // than the root.
-  // What this code does is redirect the user back to the homepage if they
-  // refresh on any nested routes.
-  useEffect(() => {
-    // if (pathname !== '/') {
-    //   history.replace('/');
-    // }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // This redirects the user back to the home page
-  // any time that the configuration changes.
-  // Note: the page that they are receives the update at the same time
-  // that this, so components may begin to render with the new data before
-  // this transition occurs.
-  // A better approach would be to perform this transition _before_ the computation
-  // is run in the first place.
-  // I could move this to `state/simulation-result.js`, but that doesn't seem very
-  // organized.
-  useEffect(
-    () => {
-      // history.push('/');
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [result]
-  );
-
-  // The /results path only exists for small screens. If you
-  // attempt to access it on a large screen, we redirect you back home.
-  useEffect(
-    () => {
-      // if (!isSmallScreen && pathname === '/results') {
-      //   history.replace('/');
-      // }
-    },
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    [isSmallScreen, pathname]
-  );
 
   return (
     <div
--- src/pages/calculator.js
+++ src/pages/calculator.js
@@ -1,15 +1,56 @@
-import React from 'react';
-import { Switch, Route, useRouteMatch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import {
+  Switch,
+  Route,
+  useRouteMatch,
+  useLocation,
+  useHistory,
+} from 'react-router-dom';
 import './calculator.css';
 import Configuration from '../configuration/configuration';
 import SimulationsOverview from '../results/simulations-overview';
 import NotFound from '../common/not-found';
 import useIsSmallScreen from '../hooks/use-is-small-screen';
 import OneSimulation from '../results/one-simulation';
+import useSimulationResult from '../state/simulation-result';
+
+const ROOT_CALCULATOR_PATH = '/calculator';
 
 export default function Calculator() {
   const isSmallScreen = useIsSmallScreen();
+  const history = useHistory();
   const { path } = useRouteMatch();
+  const { pathname } = useLocation();
+  const { result } = useSimulationResult();
+
+  // Right now, the app has no state persistence. Therefore,
+  // it doesn't make sense to allow the user to visit any other URL other
+  // than the root.
+  // What this code does is redirect the user back to the calculator root if they
+  // refresh on any nested routes.
+  useEffect(() => {
+    if (pathname !== ROOT_CALCULATOR_PATH) {
+      history.replace(ROOT_CALCULATOR_PATH);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // This redirects the user back to the home page
+  // any time that the configuration changes.
+  // Note: the page that they are receives the update at the same time
+  // that this, so components may begin to render with the new data before
+  // this transition occurs.
+  // A better approach would be to perform this transition _before_ the computation
+  // is run in the first place.
+  // I could move this to `state/simulation-result.js`, but that doesn't seem very
+  // organized.
+  useEffect(
+    () => {
+      history.push(ROOT_CALCULATOR_PATH);
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [result]
+  );
 
   const overviewPaths = isSmallScreen
     ? `${path}/results`
