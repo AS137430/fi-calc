7d41d7035fcff8ccbf4589e10d798db2e2050ad5
James Smith <jamesplease2@gmail.com>
2020-04-25 23:11:44 -0700

    Refactor to use InfoModal where appropriate

--- src/configuration/length-of-retirement-config.js
+++ src/configuration/length-of-retirement-config.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ConfigSection from './sidebar-section';
 import useForm from '../hooks/use-form';
-import Modal from '../common/modal';
+import InfoModal from '../common/info-modal';
 import Input from '../common/input';
 import useLengthOfRetirement from '../state/length-of-retirement';
 import lengthOfRetirementForm from '../form-config/length-of-retirement-form';
@@ -41,64 +41,54 @@ export default function LengthOfRetirementConfig() {
           </div>
         </ConfigSection.Contents>
       </ConfigSection>
-      <Modal
+      <InfoModal
+        title="Length of Retirement"
         active={isTitleInfoModalOpen}
         onBeginClose={() => setIsTitleInfoModalOpen(false)}>
-        <Modal.Title>Length of Retirement</Modal.Title>
-        <Modal.Body>
-          <p>
-            This is the number of years that you think your retirement might
-            last. Of course, we cannot ever know this number with certainty, but
-            we can make a reasonable guess.
-          </p>
-          <p>
-            The studies that derived the 4% rule used 30 year retirements in
-            their calculations, which is why the default length of this
-            calculator is 30 years.
-          </p>
-          <p>
-            One thing to note is that these studies were considering retirements
-            for "typically aged" retirees: folks who retire at around age 60.
-            For retirements starting at this age, you might agree that 30 years
-            is a reasonable estimate.
-          </p>
-          <p>
-            However, if you are planning to retire earlier than 60, you may
-            decide it would be more appropriate to choose a longer retirement
-            length. Feel free to do this! However, keep in mind that it is
-            important to balance the <b>number of simulations</b> that are run
-            in each calculation with your retirement length.
-          </p>
-          <p>
-            The way that this calculator works is by running simulations of
-            retirements from 1871 through to the current year. There are more 30
-            year periods between 1871 and today than there are, say, 60 year
-            periods. Consequently, you get more simulations per calculation with
-            shorter retirement lengths.
-          </p>
-          <p>
-            The more simulations that are run, the better, because that means
-            your retirement plan experiences more situations. This is why it
-            isn't useful to specify extremely long retirement lengths, like 140
-            years. With huge retirement lengths, so few simulations are run that
-            the result just isn't meaningful.
-          </p>
-          <p>
-            There is no single right answer when it comes to retirement length.
-            Many early retirees feel confident using 30 years. Hopefully, you
-            now have the information you need to choose a retirement length that
-            feels right to you.
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <button
-            className="button button-primary"
-            type="button"
-            onClick={() => setIsTitleInfoModalOpen(false)}>
-            Okay
-          </button>
-        </Modal.Footer>
-      </Modal>
+        <p>
+          This is the number of years that you think your retirement might last.
+          Of course, we cannot ever know this number with certainty, but we can
+          make a reasonable guess.
+        </p>
+        <p>
+          The studies that derived the 4% rule used 30 year retirements in their
+          calculations, which is why the default length of this calculator is 30
+          years.
+        </p>
+        <p>
+          One thing to note is that these studies were considering retirements
+          for "typically aged" retirees: folks who retire at around age 60. For
+          retirements starting at this age, you might agree that 30 years is a
+          reasonable estimate.
+        </p>
+        <p>
+          However, if you are planning to retire earlier than 60, you may decide
+          it would be more appropriate to choose a longer retirement length.
+          Feel free to do this! However, keep in mind that it is important to
+          balance the <b>number of simulations</b> that are run in each
+          calculation with your retirement length.
+        </p>
+        <p>
+          The way that this calculator works is by running simulations of
+          retirements from 1871 through to the current year. There are more 30
+          year periods between 1871 and today than there are, say, 60 year
+          periods. Consequently, you get more simulations per calculation with
+          shorter retirement lengths.
+        </p>
+        <p>
+          The more simulations that are run, the better, because that means your
+          retirement plan experiences more situations. This is why it isn't
+          useful to specify extremely long retirement lengths, like 140 years.
+          With huge retirement lengths, so few simulations are run that the
+          result just isn't meaningful.
+        </p>
+        <p>
+          There is no single right answer when it comes to retirement length.
+          Many early retirees feel confident using 30 years. Hopefully, you now
+          have the information you need to choose a retirement length that feels
+          right to you.
+        </p>
+      </InfoModal>
     </>
   );
 }
--- src/configuration/portfolio-config.js
+++ src/configuration/portfolio-config.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import IconHelp from 'materialish/icon-help';
 import ConfigSection from './sidebar-section';
 import useForm from '../hooks/use-form';
-import Modal from '../common/modal';
+import InfoModal from '../common/info-modal';
 import Input from '../common/input';
 import usePortfolio from '../state/portfolio';
 import porfolioForm from '../form-config/portfolio-form';
@@ -104,97 +104,67 @@ export default function PortfolioConfig() {
         </div>*/}
         </ConfigSection.Contents>
       </ConfigSection>
-      <Modal
+      <InfoModal
+        title="Initial Portfolio"
         active={openModal === 'titleHelp'}
         onBeginClose={() => setOpenModal(null)}>
-        <Modal.Title>Initial Portfolio</Modal.Title>
-        <Modal.Body>
-          <p>
-            Your <i>portfolio</i> is made up of all of the financial assets that
-            you own, such as stocks or bonds. In this section, you define what
-            your portfolio was or will be at the start of your retirement.
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <button
-            className="button button-primary"
-            type="button"
-            onClick={() => setOpenModal(null)}>
-            Okay
-          </button>
-        </Modal.Footer>
-      </Modal>
-      <Modal
+        <p>
+          Your <i>portfolio</i> is made up of all of the financial assets that
+          you own, such as stocks or bonds. In this section, you define what
+          your portfolio was or will be at the start of your retirement.
+        </p>
+      </InfoModal>
+      <InfoModal
+        title="Equities"
         active={openModal === 'equities'}
         onBeginClose={() => setOpenModal(null)}>
-        <Modal.Title>Equities</Modal.Title>
-        <Modal.Body>
-          <p>
-            Equities are also known as stocks. For example, if you own shares of{' '}
-            <a
-              href="https://investor.vanguard.com/mutual-funds/profile/VTSAX"
-              rel="noopener noreferrer"
-              target="_blank">
-              VTSAX
-            </a>
-            , then you would place the value of those shares here.
-          </p>
-          <p>
-            This calculator only supports a single value for all of your
-            equities, so add them all up to get the value to input here.
-          </p>
-          <p>
-            This calculator uses the S&P 500's historical performance data from{' '}
-            <a
-              href="http://www.econ.yale.edu/~shiller/data.htm"
-              target="_blank"
-              rel="noreferrer noopener">
-              Robert Shiller
-            </a>{' '}
-            for this value.
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <button
-            className="button button-primary"
-            type="button"
-            onClick={() => setOpenModal(null)}>
-            Okay
-          </button>
-        </Modal.Footer>
-      </Modal>
-      <Modal
+        <p>
+          Equities are also known as stocks. For example, if you own shares of{' '}
+          <a
+            href="https://investor.vanguard.com/mutual-funds/profile/VTSAX"
+            rel="noopener noreferrer"
+            target="_blank">
+            VTSAX
+          </a>
+          , then you would place the value of those shares here.
+        </p>
+        <p>
+          This calculator only supports a single value for all of your equities,
+          so add them all up to get the value to input here.
+        </p>
+        <p>
+          This calculator uses the S&P 500's historical performance data from{' '}
+          <a
+            href="http://www.econ.yale.edu/~shiller/data.htm"
+            target="_blank"
+            rel="noreferrer noopener">
+            Robert Shiller
+          </a>{' '}
+          for this value.
+        </p>
+      </InfoModal>
+      <InfoModal
+        title="Annual Fees"
         active={openModal === 'fees'}
         onBeginClose={() => setOpenModal(null)}>
-        <Modal.Title>Annual Fees</Modal.Title>
-        <Modal.Body>
-          <p>
-            Most equities have a yearly fee that you must pay to own the stock.
-            Sometimes, it is called an expense ratio. The default value is
-            0.04%, which was the expense ratio for{' '}
-            <a
-              href="https://investor.vanguard.com/mutual-funds/profile/VTSAX"
-              rel="noopener noreferrer"
-              target="_blank">
-              VTSAX
-            </a>{' '}
-            in April 2020.
-          </p>
-          <p>
-            Because this calculator only supports a single input for all of your
-            equities, we recommend choosing an average fee to represent all of
-            your holdings.
-          </p>
-        </Modal.Body>
-        <Modal.Footer>
-          <button
-            className="button button-primary"
-            type="button"
-            onClick={() => setOpenModal(null)}>
-            Okay
-          </button>
-        </Modal.Footer>
-      </Modal>
+        <p>
+          Most equities have a yearly fee that you must pay to own the stock.
+          Sometimes, it is called an expense ratio. The default value is 0.04%,
+          which was the expense ratio for{' '}
+          <a
+            href="https://investor.vanguard.com/mutual-funds/profile/VTSAX"
+            rel="noopener noreferrer"
+            target="_blank">
+            VTSAX
+          </a>{' '}
+          in April 2020.
+        </p>
+        <p>
+          Because this calculator only supports a single input for all of your
+          equities, we recommend choosing an average fee to represent all of
+          your holdings.
+        </p>
+      </InfoModal>
     </>
   );
 }
