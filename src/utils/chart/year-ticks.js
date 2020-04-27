12dbe94ac65b6c6e6bd26988a84913e0e4736963
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-04-27 08:50:03 -0700

    Update code comment

--- src/utils/chart/year-ticks.js
+++ src/utils/chart/year-ticks.js
@@ -2,9 +2,9 @@
 // This can be interpreted as meaning:
 // the spacing between each tick can be 1 year, OR 2 years,
 // OR 3 years, and so on, but never a value not in this list.
-// For instance, it would never be 5 years.
+// For instance, there will never be 6 years between ticks.
 //
-// If/when I change this app to support months, then I would want
-// to adjust this to be units of 12 (i.e.; 12, 24, 36)
+// If/when I update this app to support months, then I would want
+// to instead use multiples of 12 (i.e.; 12, 24, 36)
 //
 export default [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 35, 40, 50];
