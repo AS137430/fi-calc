bf6e198de2c0f20229976242fa025f2f656a93ba
James Smith <jamesplease2@gmail.com>
2020-04-28 18:21:00 -0700

    Update chart active point to use blue

--- src/vendor/chart/components/active-data-point.js
+++ src/vendor/chart/components/active-data-point.js
@@ -17,12 +17,12 @@ export default function ActiveDataPoint({
   return (
     <>
       <circle cx={point[0]} cy={point[1]} r="8px" fill="#141414" />
-      <circle cx={point[0]} cy={point[1]} r="5px" fill="var(--accentColor)" />
+      <circle cx={point[0]} cy={point[1]} r="5px" fill="var(--primaryBlue)" />
       <path
         d={`M${point[0]} ${topMargin} v ${dataForRender.svgElement.viewBox[1] -
           svgXAxisLabelsHeight -
           topMargin}`}
-        stroke="var(--accentColor)"
+        stroke="var(--primaryBlue)"
         strokeWidth="2px"
         fill="transparent"
       />
