5f40e0d34448aa9109aefa69abfc03c3f222caca
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-05-03 14:13:43 -0700

    Begin adding GK tests

--- src/vendor/@moolah/simulation-engine/withdrawal-strategies/guyton-klinger.test.js
+++ src/vendor/@moolah/simulation-engine/withdrawal-strategies/guyton-klinger.test.js
@@ -1,7 +1,85 @@
 import guytonKlinger from './guyton-klinger';
 
 describe('guytonKlinger', () => {
-  it('is a placeholder', () => {
-    expect(typeof guytonKlinger === 'function').toBe(true);
+  describe('first year', () => {
+    it('returns the initial result', () => {
+      const withdrawal = guytonKlinger({
+        yearsRemaining: 30,
+        inflation: 1,
+        isFirstYear: true,
+        cpi: 100,
+        previousYearCpi: 100,
+        portfolioTotalValue: 1000000,
+        gkInitialWithdrawal: 40000,
+        firstYearStartPortolioTotalValue: 1000000,
+
+        stockMarketGrowth: 0.03,
+        previousYearBaseWithdrawalAmount: 0,
+      });
+
+      expect(withdrawal).toEqual({
+        value: 40000,
+        meta: {
+          modifiedWithdrawalRuleApplied: false,
+          capitalPreservationRuleApplied: false,
+          prosperityRuleApplied: false,
+        },
+      });
+    });
+
+    it('returns the initial result, even for large % withdrawals', () => {
+      const withdrawal = guytonKlinger({
+        yearsRemaining: 30,
+        inflation: 1,
+        isFirstYear: true,
+        cpi: 100,
+        previousYearCpi: 100,
+        portfolioTotalValue: 1000000,
+        gkInitialWithdrawal: 900000,
+        firstYearStartPortolioTotalValue: 1000000,
+
+        stockMarketGrowth: 0.03,
+        previousYearBaseWithdrawalAmount: 0,
+      });
+
+      expect(withdrawal).toEqual({
+        value: 900000,
+        meta: {
+          modifiedWithdrawalRuleApplied: false,
+          capitalPreservationRuleApplied: false,
+          prosperityRuleApplied: false,
+        },
+      });
+    });
+  });
+
+  describe('> 1 years', () => {
+    // Three conditions:
+    describe('Modified Withdrawal Rule', () => {
+      it('applies when all of the conditions are met', () => {
+        const withdrawal = guytonKlinger({
+          yearsRemaining: 25,
+          inflation: 1.02,
+          isFirstYear: false,
+          cpi: 103,
+          previousYearCpi: 100,
+          portfolioTotalValue: 1200000,
+          gkInitialWithdrawal: 40000,
+          firstYearStartPortolioTotalValue: 1000000,
+
+          stockMarketGrowth: -0.05,
+          previousYearBaseWithdrawalAmount: 45500,
+        });
+
+        expect(withdrawal).toEqual({
+          value: 45500,
+          meta: {
+            modifiedWithdrawalRuleApplied: true,
+            capitalPreservationRuleApplied: false,
+            prosperityRuleApplied: false,
+          },
+        });
+      });
+    });
   });
 });
