3437637397d0d3b9b53e6545d2ad980ec5756b68
James Smith <jamesplease2@gmail.com>
2020-05-02 21:58:42 -0700

    95% rule tests

--- src/vendor/@moolah/simulation-engine/withdrawal-strategies/ninety-five-percent-rule.test.js
+++ src/vendor/@moolah/simulation-engine/withdrawal-strategies/ninety-five-percent-rule.test.js
@@ -8,7 +8,7 @@ describe('ninetyFive', () => {
           isFirstYear: true,
 
           firstYearStartPortolioTotalValue: 100000,
-          portfolioTotalValue: 10000,
+          portfolioTotalValue: 100000,
           initialWithdrawalRate: 0.04,
 
           previousYearWithdrawalAmount: 0,
@@ -22,7 +22,7 @@ describe('ninetyFive', () => {
           isFirstYear: true,
 
           firstYearStartPortolioTotalValue: 100000,
-          portfolioTotalValue: 10000,
+          portfolioTotalValue: 100000,
           initialWithdrawalRate: 0.04,
 
           previousYearWithdrawalAmount: 0,
@@ -39,10 +39,74 @@ describe('ninetyFive', () => {
           isFirstYear: true,
 
           firstYearStartPortolioTotalValue: 100000,
-          portfolioTotalValue: 10000,
+          portfolioTotalValue: 100000,
           initialWithdrawalRate: 0.04,
 
           previousYearWithdrawalAmount: 0,
+
+          maxWithdrawal: 1000,
+          inflation: 2,
+        })
+      ).toBe(2000);
+    });
+  });
+
+  describe('not first year', () => {
+    it('uses withdrawal % of current portfolio when it is more than 95% of last year', () => {
+      expect(
+        ninetyFivePercentRule({
+          isFirstYear: false,
+
+          firstYearStartPortolioTotalValue: 100000,
+          portfolioTotalValue: 200000,
+          initialWithdrawalRate: 0.04,
+
+          previousYearWithdrawalAmount: 4000,
+        })
+      ).toBe(8000);
+    });
+
+    it('uses 95% of last year when its more than % of current portfolio', () => {
+      expect(
+        ninetyFivePercentRule({
+          isFirstYear: false,
+
+          firstYearStartPortolioTotalValue: 100000,
+          portfolioTotalValue: 50000,
+          initialWithdrawalRate: 0.04,
+
+          previousYearWithdrawalAmount: 4000,
+        })
+      ).toBe(3800);
+    });
+
+    it('respects min withdrawal', () => {
+      expect(
+        ninetyFivePercentRule({
+          isFirstYear: false,
+
+          firstYearStartPortolioTotalValue: 100000,
+          portfolioTotalValue: 200000,
+          initialWithdrawalRate: 0.04,
+
+          previousYearWithdrawalAmount: 4000,
+
+          minWithdrawal: 10000,
+          inflation: 2,
+        })
+      ).toBe(20000);
+    });
+
+    it('respects max withdrawal', () => {
+      expect(
+        ninetyFivePercentRule({
+          isFirstYear: false,
+
+          firstYearStartPortolioTotalValue: 100000,
+          portfolioTotalValue: 200000,
+          initialWithdrawalRate: 0.04,
+
+          previousYearWithdrawalAmount: 4000,
 
           maxWithdrawal: 1000,
           inflation: 2,
