38e992a16c426faba80d28aa27e937e4bc14a9d0
James Smith <2322305+jamesplease@users.noreply.github.com>
2020-05-03 23:21:44 -0700

    Tests for runSimulation

---
+++ src/vendor/@moolah/simulation-engine/run-simulation.test.ts
@@ -0,0 +1,7 @@
+import runSimulation from './run-simulation';
+
+describe('runSimulation', () => {
+  it('should be a function', () => {
+    expect(typeof runSimulation === 'function').toBe(true);
+  });
+});
